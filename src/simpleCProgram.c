27d77c2e0d1af2f750b94ab4477f376ab80e9c73
chadd williams <will4614@pacificu.edu>
2021-09-02 11:25:08 -0700

    clean up includes

--- src/simpleCProgram.c
+++ src/simpleCProgram.c
@@ -8,16 +8,9 @@
 //***************************************************************************
 #define _GNU_SOURCE
 
-#include <sys/types.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
-#include <netdb.h>
-#include <unistd.h>
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
 
 
 /****************************************************************************
